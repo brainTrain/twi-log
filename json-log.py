094d25085cdf42364467ae870ad51d3f651e4ab1
Brain Train (╯°□°)╯ ︵ ┻━┻ <abductee@gmail.com>
2012-12-13 23:54:12 +0000

    now this will run forever with a sleep timer!

--- json-log.py
+++ json-log.py
@@ -18,70 +18,77 @@ LOGGLY_URL_BOSSEMAILS = 'https://logs.loggly.com/inputs/7733d7fd-bf5f-445c-a702-
 INSERT_HTTP = httplib2.Http(timeout=10)
 
 while 1:
-    #init json_blob
-    json_blob_0 = {
-        'user-s': None,
-        'person+s': None,
-        'red*s': None,
-        'blue': None,
-        'green': None,
-        'yellow': None
-    }
-    json_blob_1 = {
-        'whatever': None,    
-        'dude': None,    
-        'like': None,    
-        'literally': None,    
-    }
-    for val in json_blob_0:        
-        string_int_switch = random.randint(0,1)
-        #random_graph_number = random.uniform(400.99199,400.99899)
-        random_graph_number = random.randint(200,600)
-        random_string = base64.urlsafe_b64encode(os.urandom(15)) 
-        if string_int_switch:
-            random_val = random_string
-        else:
-            random_val = random_graph_number
+    random_count = random.randint(2000,5000)
+    counter = 0
+    while counter <= random_count:
+        #init json_blob
+        json_blob_0 = {
+            'user': None,
+            'person': None,
+            'red': None,
+            'blue': None,
+            'green': None,
+            'yellow': None
+        }
+        json_blob_1 = {
+            'whatever': None,    
+            'dude': None,    
+            'like': None,    
+            'literally': None,    
+        }
+        for val in json_blob_0:        
+            string_int_switch = random.randint(0,1)
+            #random_graph_number = random.uniform(400.99199,400.99899)
+            random_graph_number = random.randint(200,600)
+            random_string = base64.urlsafe_b64encode(os.urandom(15)) 
+            if string_int_switch:
+                random_val = random_string
+            else:
+                random_val = random_graph_number
 
-        json_blob_0[val] = random_val
+            json_blob_0[val] = random_val
 
-    json_blob_0 = json.dumps(json_blob_0)
+        json_blob_0 = json.dumps(json_blob_0)
 
-    for val in json_blob_1:        
-        string_int_switch = random.randint(0,1)
-        #random_graph_number = random.uniform(400.99199,400.99899)
-        random_graph_number = random.randint(-100,1)
-        random_string = base64.urlsafe_b64encode(os.urandom(15)) 
-        if string_int_switch:
-            #random_val = random_string
-            random_val = random_graph_number
-        else:
-            random_val = random_graph_number
+        for val in json_blob_1:        
+            string_int_switch = random.randint(0,1)
+            #random_graph_number = random.uniform(400.99199,400.99899)
+            random_graph_number = random.randint(-100,1)
+            random_string = base64.urlsafe_b64encode(os.urandom(15)) 
+            if string_int_switch:
+                #random_val = random_string
+                random_val = random_graph_number
+            else:
+                random_val = random_graph_number
 
-        json_blob_1[val] = random_val
+            json_blob_1[val] = random_val
 
-    json_blob_1 = json.dumps(json_blob_1)
+        json_blob_1 = json.dumps(json_blob_1)
 
-    #logging to office
-    try:
-        #resp, content = INSERT_HTTP.request(LOGGLY_URL_BOSSEMAILS, 
-        #resp, content = INSERT_HTTP.request(LOGGLY_URL_GUNROCK_NUMBER, 
-        resp, content = INSERT_HTTP.request(LOGGLY_URL_GUNROCK, 
-                                            'POST', 
-                                            body=json_blob_0, 
-                                            headers={'content-type':'application/json'})
-        resp, content = INSERT_HTTP.request(LOGGLY_URL_GUNROCK, 
-                                            'POST', 
-                                            body=json_blob_1, 
-                                            headers={'content-type':'application/json'})
-        print 'json_blob_0'
-        print json_blob_0
-        print 'json_blob_1'
-        print json_blob_1
-    # if there's a timeout exception, print some info, and pass
-    except socket.timeout:
-        print 'hmm timeout... printing tweet, then on to the next one'
+        #logging to office
+        try:
+            #resp, content = INSERT_HTTP.request(LOGGLY_URL_BOSSEMAILS, 
+            #resp, content = INSERT_HTTP.request(LOGGLY_URL_GUNROCK_NUMBER, 
+            resp, content = INSERT_HTTP.request(LOGGLY_URL_GUNROCK, 
+                                                'POST', 
+                                                body=json_blob_0, 
+                                                headers={'content-type':'application/json'})
+            resp, content = INSERT_HTTP.request(LOGGLY_URL_GUNROCK, 
+                                                'POST', 
+                                                body=json_blob_1, 
+                                                headers={'content-type':'application/json'})
+            print 'json_blob_0'
+            print json_blob_0
+            print 'json_blob_1'
+            print json_blob_1
+        # if there's a timeout exception, print some info, and pass
+        except socket.timeout:
+            print 'hmm timeout... printing tweet, then on to the next one'
 
-        pass
-    
-    #time.sleep(0.4)
+            pass
+        # update counter
+        counter = counter + 1
+    SLEEP_TIME = 900
+    print "sleeping for %s minutes" % (SLEEP_TIME/60)
+    # sleep for 15 minutes 
+    time.sleep(SLEEP_TIME)
